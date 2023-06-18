648f69cd04c492a73065ec5ed0665a0ddfc45712
ignaskmn <ignas@kmn.lt>
2023-06-18 10:33:02 +0300

    no-unescaped-entities fix

--- components/referals/ReferralsForm.tsx
+++ components/referals/ReferralsForm.tsx
@@ -8,8 +8,8 @@ export default function ReferralsForm() {
         <div className={styles.content}>
           <h1>refer friends and get rewards</h1>
           <p>
-            Refer your friends to us and earn hotel booking vouchers. We'll give
-            you 1 coin for each friend that installs our extension. Minimum
+            Refer your friends to us and earn hotel booking vouchers. We&apos;ll
+            give you 1 coin for each friend that installs our extension. Minimum
             cash-out at 20 coins.
           </p>
           <ReferralsInput />
